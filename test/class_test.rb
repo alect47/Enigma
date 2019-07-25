8ae8ff44be4dfadbb6e3cb1838ec071e4fff8d14
Alec Wells <alec.wells47@gmail.com>
2019-07-25 06:15:44 -0600

    Initial commit

---
+++ test/class_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper'
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/classes/class'
+require 'pry'
+
+class ClassTest < Minitest::Test
+
+end
