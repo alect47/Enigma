c3faa619a862117580d9be7c6223c56f225470b7
Alec Wells <alec.wells47@gmail.com>
2019-07-26 17:21:53 -0600

    Stub out key test

--- lib/key.rb
+++ lib/key.rb
@@ -1,17 +1,17 @@
 class Key
-  attr_reader :given_key
+  attr_reader :number
 
-  def initialize(given_key = "12345")
-    @given_key = given_key
-  end
+    def initialize(number = "12345")
+      @number = number
+    end
 
   def generate_random
     random = (rand(1..100000)).to_s
-    @given_key = ("0" * (5-(random.length)) + random)
+    @number = ("0" * (5-(random.length)) + random)
   end
 
   def slice_num
-    @given_key.to_s.split('')
+    @number.to_s.split('')
   end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,27 +1,18 @@
 require './test/test_helper'
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require './lib/key'
-# require 'pry'
-# require 'mocha/minitest'
 
 class KeyTest < Minitest::Test
 
   def setup
-    @key = Key.new("12345")
+    @key = Key.new
   end
 
   def test_key_exists
     assert_instance_of Key, @key
   end
 
-  def test_attributes
-    assert_instance_of String, @key.given_key
-  end
-
   def test_generate_random
-    @key.stubs(:generate_random).returns("12345")
-    assert_equal "12345", @key.given_key
+    @key.stubs(:generate_random).returns("13579")
+    assert_equal "13579", @key.generate_random
   end
 
   # def test_can_slice_number
