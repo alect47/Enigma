866a84f65ad5f0e133b740686c8e61c8bad618d5
Alec Wells <alec.wells47@gmail.com>
2019-07-25 20:22:20 -0600

    Tentative method for current date

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -10,5 +10,14 @@ class OffsetTest < Minitest::Test
     @offset = Offset.new
   end
 
+  def test_offset_exists
+    assert_instance_of Offset, @offset
+  end
+
+  def test_can_find_current_date
+    @offset.current_date
+    assert_equal 250719, @offset.date
+  end
+
 
 end
