5ef9fba8600ae83e8e213c61e81500682a13f6d2
Alec Wells <alec.wells47@gmail.com>
2019-07-26 18:25:17 -0600

    Modify offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -3,21 +3,21 @@ require 'date'
 class Offset
   attr_reader :date
 
-  def initialize(date = 250718)
+  def initialize(date = "250719")
     @date = date
   end
 
   def current_date
     cur_date = DateTime.now
-    @date = cur_date.strftime("%d/%m/%y").gsub("/","").to_i
+    @date = cur_date.strftime("%d/%m/%y").gsub("/","")
   end
 
   def square_date
-    @date * @date
+    (@date.to_i * @date.to_i).to_s
   end
 
   def make_keys
-    square_date.to_s.split('')[-4..-1].map(&:to_i)
+    square_date.split('')[-4..-1].map(&:to_i)
   end
 
 
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,8 +1,4 @@
 require './test/test_helper'
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require './lib/offset'
-# require 'pry'
 
 class OffsetTest < Minitest::Test
 
@@ -16,11 +12,13 @@ class OffsetTest < Minitest::Test
 
   def test_can_find_current_date
     @offset.current_date
-    assert_equal 260719, @offset.date
+    assert_equal "260719", @offset.date
   end
 
   def test_can_square_date
-    assert_equal 62859515524, @offset.square_date
+    # @offset.stubs(:current_date).returns("260719")
+    
+    assert_equal "62860016961", @offset.square_date
   end
 
   def test_can_take_make_keys
