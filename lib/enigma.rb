d3edf452f861de3db0433b88d858af067365875f
Alec Wells <alec.wells47@gmail.com>
2019-07-27 15:37:56 -0600

    Start work on enigma initialize and attributes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,11 @@
+require 'date'
+
 class Enigma
-  attr_reader
+  attr_reader :key, :offset, :shift
 
   def initialize
+    @key = Key.new
+    @offset = Offset.new
+    @shift = Shift.new(@key, @offset)
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,17 +1,23 @@
 require './test/test_helper'
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require './lib/enigma'
-# require 'pry'
 
 class EnigmaTest < Minitest::Test
 
   def setup
+    # @key = Key.new
+    # @offset = Offset.new
+    # @shift = Shift.new(@key, @offset)
     @enigma = Enigma.new
   end
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_attributes
+    assert_instance_of Key, @enigma.key
+    assert_instance_of Offset, @enigma.offset
+    assert_instance_of Shift, @enigma.shift
+
   end
 
 end
